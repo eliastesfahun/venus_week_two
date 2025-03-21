fc2ab3e77fe1957b88e049ac8ebb8feed78df344
elias <eltes2010@gmail.com>
2025-03-21 19:47:23 +0100

    added index html

---
+++ sample.js
@@ -0,0 +1,113 @@
+// conditions
+
+let age = 2
+if (age >= 18) {
+    console.log("you are allowed to vote")
+} else {
+    console.log("you are too young too vote")
+}
+
+
+let name = "jhon"
+
+if (name === "abebe") {
+    console.log("welcome abebe")
+} else {
+    console.log("you are not abebe")
+}
+
+
+let a = 12
+let b = 1
+
+if (a == b) {
+    console.log("a is equal with b")
+} else if (a > b) {
+    console.log("a is greater than b")
+} else {
+    console.log("a is less than b")
+}
+
+let grade = 62
+
+if (grade >= 90) {
+    console.log("A+")
+} else if (grade >= 80) {
+    console.log("A")
+} else if (grade >= 75) {
+    console.log("A_")
+} else if (grade >= 70) {
+    console.log("B+")
+} else if (grade >= 65) {
+    console.log("B")
+} else if (grade >= 60) {
+    console.log("B-")
+} else {
+    console.log("Failed")
+}
+
+
+let time = 12
+
+if (time >= 6 && time < 18) {
+    console.log("it is daytime")
+} else {
+
+    console.log("its nighttime")
+}
+
+
+let number = 6
+
+if (number % 2 === 0) {
+    console.log("the number is even")
+
+} else {
+    console.log("the number is odd")
+}
+
+
+let food = "small"
+
+if (food === "big") {
+    conslole.log("pay for three")
+
+} else if (food === "medium") {
+    console.log("pay for two")
+} else {
+    console.log("pay for one")
+}
+
+let day = "monday"
+if (day === "monday") {
+    console.log("today is monday")
+} else if (day === "thuesday") {
+    console.log("today is tuesday")
+} else if (day === "wednsday") {
+    console.log("today is wednsday")
+} else if (day === "thursday") {
+    console.log("today is thursday")
+} else if (day === "friday") {
+    console.log("today is friday")
+} else if (day === "saturday") {
+    console.log("today is saturday")
+} else {
+    console.log("today is sunday")
+}
+
+
+
+let lightColor = "green"
+
+if (lightColor === "green") {
+    console.log("go")
+}
+else if (lightColor === "yellow") {
+    console.log("slow down")
+} else if (lightColor === "red") {
+    console.log("stop")
+}
+else {
+    console.log("invalid color")
+}
+
